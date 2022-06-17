641d27dda2503793eac4c2f805b1ea86c1bb6ef8
Jaho <kjh13650@gmail.com>
2022-06-17 23:01:48 +0900

    변수와 상수

---
+++ HelloWolrd/js/let_const.js
@@ -0,0 +1,22 @@
+
+// 변수 가지고 놀기
+let admin,name;
+
+name = 'John';
+admin = name;
+
+alert(admin);
+
+// 올바르 이름 선택하기
+// 내가 낸 답
+let planets = "earth";
+const userName = "user";
+//해답
+// planet이라는 간단한 이름을 지어줘도 되지만, 
+//이렇게 하면 어떤 행성(planet)을 언급하는 것인지 모호합니다. 변수명이 아주 길어지지 않는 이상, 설명을 좀 더 곁들어주는 것이 좋습니다.
+let ourPlanetName = "Earth";
+
+let currentUserName = "John";
+// 여기서도 마찬가지로 userName같이 이름을 짧게 지어주기보다는,
+// 현재 접속 중인 사용자라는 정보를 담고 있는 이름으로 지어주는 것이 좋습니다.
+
