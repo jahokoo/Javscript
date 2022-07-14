5211bbd94f76484997b92d95400ecba492de2e7b
Jaho <kjh13650@gmail.com>
2022-07-15 00:27:43 +0900

    문자열 part2

--- HelloWolrd/data_structure/string.js
+++ HelloWolrd/data_structure/string.js
@@ -97,8 +97,173 @@
 // str[0] = 'z'; 
 // console.log(str[0]) // h  // 변경 내용을 무시함
 
-let str = 'hi';
+// let str = 'hi';
 
-str = 'z' + str[1]; // 문자열 전체를 교체함
+// str = 'z' + str[1]; // 문자열 전체를 교체함
 
-console.log( str ); // zi
+// console.log( str ); // zi
+
+
+// 대-소문자 변경하기
+
+// 소문자로 변경해주는 toLowerCase
+// console.log('Interface'.toLowerCase()); // interface
+
+// // 대문자로 변경해주는 toUpperCase
+// console.log('Interface'.toUpperCase()); // INTERFACE
+
+// // 글자 하나의 케이스 변경하기
+// console.log('Interface'[0].toLowerCase()); // i
+
+// 부분 문자열 찾기
+
+// indexOf를 이용하여 부분 문자열의 위치를 찾아보자.
+// let str = 'Widget with id';
+
+// alert( str.indexOf('Widget') ); // 0, str은 'Widget'으로 시작함
+// alert( str.indexOf('widget') ); // -1, indexOf는 대·소문자를 따지므로 원하는 문자열을 찾지 못함
+
+// alert( str.indexOf("id") ); // 1, "id"는 첫 번째 위치에서 발견됨 (Widget에서 id)
+// // indexOf의 2번째 인수를 작성해 사용해 보자.
+// alert(str.indexOf('id',2)); // 12
+
+
+// let str = 'As sly as a fox, as strong as an ox';
+
+// let target = 'as'; // as를 찾아보자.
+
+// let pos = 0;
+// while(true){
+//     let foundPos = str.indexOf(target,pos);
+//     if(foundPos == -1) break;
+
+//     alert(`위치 : ${foundPos}`);
+//     pos = foundPos + 1; // 다음 위치를 기준으로 검색을 이어간다.
+// }
+
+// let str = 'As sly as a fox, as strong as an ox';
+
+// let target = 'as';
+
+// let pos = -1;
+// while((pos = str.indexOf(target,pos + 1)) != -1){
+// 	alert(`위치 : ${pos}`);
+// }
+
+// let str = 'Widget with id';
+
+// // Widget의 위치는 0 이다.
+// // if문에서 0은 'false'로 간주하기 때문에 console.log는 반환되지 않는다.
+// if(str.indexOf('Widget')){
+//     console.log('찾음') // 의도한 대로 동작하지 않는다.
+// }
+
+// // 따라서 0 이 아닌 -1과 비교를 해야한다.
+// if (str.indexOf('Widget') != -1){
+//     console.log('찾았다...') // 의도한 대로 동작한다.
+// }
+
+
+// 비트 NOT 연산자
+// 피연산자를 32비트 정수로 바꾼 후 모든 비트를 반전한다.
+// alert( ~2 ); // -3, -(2+1)과 같음
+// alert( ~1 ); // -2, -(1+1)과 같음
+// alert( ~0 ); // -1, -(0+1)과 같음
+// alert( ~-1 ); // 0, -(-1+1)과 같음
+
+// 위에서 -1과 비교한 코드 대신 NOT연산자를 이용해 더 짧게 구현할 수 있다.
+
+// let str = "Widget";
+
+// if(~str.indexOf("Widget")){
+//     alert('찾았다.'); // 잘 동작한다.
+// }
+
+
+// 부분 문자열을 포함하고 있는지 알려주는 includes
+
+// console.log( "Widget with id".includes("Widget") ); // true
+// console.log( "Hello".includes("Bye") ); // false
+
+// // indexOf와 마찬가지로 문자열 검색 위치를 설정할 수 있다.
+// alert( "Widget".includes("id") ); // true
+// alert( "Widget".includes("id", 3) ); // false, 세 번째 위치 이후엔 "id"가 없습니다.
+
+// // 특정 문자열로 시작하는지 여부를 확인하는 startsWith()
+// // 특정 문자열로 끝나는지 여부를 확인하는 endWith()
+// alert( "Widget".startsWith("Wid") ); // true, "Widget"은 "Wid"로 시작합니다.
+// alert( "Widget".endsWith("get") ); // true, "Widget"은 "get"으로 끝납니다.
+
+
+// 부분 문자열 추출하기
+// slice()를 이용하여 문자열 추출하기
+
+// let str = 'stringify';
+// console.log(str.slice(0,5)); // strin , 5번째 위치는 포함되지 않는다. (0부터 4번째 위치까지만)
+// console.log(str.slice(0,1)); // s , 마찬가지로 1번째 위치는 제외 
+
+// // 두 번째 인수를 생략할 경우 시작 위치 부터 문자열 끝까지 반환한다.
+
+// console.log(str.slice(2)); // ringify, 2번째 부터 끝까지
+
+
+// // 끝에서 4번째부터 시작해 끝에서 1번째 위치 까지 반환
+// console.log(str.slice(-4,-1)); //gif 
+
+
+
+// substring()을 이용하여 문자열 추출하기
+// let str = 'stringify';
+
+// // 동일한 부분 문자열을 반환한다.
+// console.log(str.substring(2,6)); // ring
+// console.log(str.substring(6,2)); // ring
+// // substring은 음수 인수를 허용하지 않는다. 음수는 0으로 처리한다.
+
+// // slice를 사용하면 다른 결과가 나온다.
+// console.log(str.slice(2,6)); // ring
+// console.log(str.slice(6,2)); // '' (빈 문자열)
+
+
+
+// substr()을 이용해 문자열 추출하기
+// substr은 길이를 기준으로 문자열을 추출한다.
+
+// let str =  'stringify';
+// console.log(str.substr(2,4)); // ring , 2부터 글자 4개를 반환 
+
+// // 음수를 사용하여 뒤에서 부터 추출할 수 있다.
+// console.log(str.substr(-4,2)); // gi, 끝에서 4번째 위치부터 2글자 추출
+
+
+// 문자열 비교하기
+
+// 문자열을 비교할 떄 알파벳 순서를 기준으로 비교가 이뤄지는데 이상한 특징이 몇 가지 있다.
+
+// 소문자는 대문자 보다 항상 크다.
+// alert( 'a' > 'Z' ); // true
+
+// // 발음 구별 기호가 붙은 문자는 알파벳 순서 기준을 따르지 않는다.
+// alert( 'Österreich' > 'Zealand' ); // true (Österreich는 오스트리아를 독일어로 표기한 것임
+
+// //codePointAt()을 이용해 글자에 연관된 코드를 알아보자.
+
+// // 글자는 같지만 케이스는 다르므로 변환되는 코드가 다르다.
+// alert( "z".codePointAt(0) ); // 122
+// alert( "Z".codePointAt(0) ); // 90
+
+// String.fromCodePoint()를 이용하여 숫자형식의 코드를 대응하는 글자를 확인해 보자.
+// console.log(String.fromCodePoint(90)); // Z
+
+// \u 뒤에 16진수로 특정글자를 만들수 있다.
+// 90을 16진수로 변환하면 5a입니다.
+// alert( '\u005a' ); // Z
+
+let str = '';
+
+for (let i = 65; i <= 220; i++) {
+  str += String.fromCodePoint(i);
+}
+console.log( str );
+// ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~
+// ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜ
